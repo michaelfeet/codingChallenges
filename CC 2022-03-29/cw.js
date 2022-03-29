926f5f872075d2df835b333f3722403ac9ca7d6b
Michael Feet <michael.a.feet@gmail.com>
2022-03-29 16:58:31 -0400

    Coding challenge and solution for 3/29/2022

---
+++ CC 2022-03-29/cw.js
@@ -0,0 +1,15 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// given boon
+
+// return string
+
+function booleanToString(b){
+    // change bool to string
+    return b.toString()
+}
+console.log(booleanToString(true)) // 'true'
+
+// let booleanToString = b => b.toString()
