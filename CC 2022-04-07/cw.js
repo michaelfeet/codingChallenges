56f2c2748be4b4644646a97d66d16abea4407595
Michael Feet <michael.a.feet@gmail.com>
2022-04-07 16:14:28 -0400

    Coding challenge and solution for 4/7/2022

---
+++ CC 2022-04-07/cw.js
@@ -0,0 +1,15 @@
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+//takes in a number. always a number. positive, negative, decimal.
+
+//returns a str
+
+let numberToString = num => num.toString()
+
+console.log(numberToString(123)) // '123'
