33dc52f7f21cca02a4ef6d0013ac23a83158cc36
Michael Feet <michael.a.feet@gmail.com>
2022-05-13 16:04:48 -0400

    Coding challenge and solution for 5/13/2022

---
+++ CC 2022-05-13/cw.js
@@ -0,0 +1,11 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+}
+
+// take in integer, double, return
+
+console.log(doubleInteger(2)) // 4
+console.log(doubleInteger(4)) // 8
