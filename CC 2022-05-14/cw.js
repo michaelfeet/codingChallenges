0fae2aa394fa8b258d749401f153c282e22e48e1
Michael Feet <michael.a.feet@gmail.com>
2022-05-14 23:34:08 -0400

    Coding challenge and solution for 5/14/2022

---
+++ CC 2022-05-14/cw.js
@@ -0,0 +1,18 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+// function repeatStr (n, s) {
+//     return '';
+//   }
+
+let repeatStr = (n, s) => {
+    let str = ''
+    for (let i = 1; i <= n; i++) {
+        str += s
+    }
+    return str
+}
+
+console.log(repeatStr(5, 'I'))
