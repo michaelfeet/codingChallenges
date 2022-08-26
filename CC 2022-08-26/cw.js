267572d96e0befc1c3194b115b176b661b439bb9
Michael Feet <michael.a.feet@gmail.com>
2022-08-26 06:49:49 -0400

    8 kyu get character from ASCII Value

---
+++ CC 2022-08-26/cw.js
@@ -0,0 +1,16 @@
+// 8 kyu get character from ASCII Value
+
+// DESCRIPTION:
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+let getChar = (n) => String.fromCharCode(n);
+
+console.log(getChar(65));
