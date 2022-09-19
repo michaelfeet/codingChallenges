0813b4b101a68c0747515063a961129fa09a7dba
Michael Feet <michael.a.feet@gmail.com>
2022-09-19 06:47:07 -0400

    8kyu 5 without numbers

---
+++ CC 2022-09-18/cw.js
@@ -0,0 +1,11 @@
+// 8kyu 5 without numbers
+
+// DESCRIPTION:
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+const unusualFive = () => 'abcde'.length
+console.log(unusualFive())
