ddc48c672300d327642990192587c2538ea6ec53
Michael Feet <michael.a.feet@gmail.com>
2022-09-24 10:34:04 -0400

    Take the First N Elements

---
+++ CC 2022-09-24/cw.js
@@ -0,0 +1,12 @@
+// 8kyu Enumerable Magic #25 - Take the First N Elements
+
+// DESCRIPTION:
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+// If you need help, here's a reference:
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+let function1 = (arr, n) => arr.slice(0,n)
+
+console.log(function1([1,2,3,4,5,1,2,3,4], 3))
