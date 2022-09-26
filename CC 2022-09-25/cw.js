6a9b4e0a946f901105487f6b2de224fcd40d2c9e
Michael Feet <michael.a.feet@gmail.com>
2022-09-25 20:13:43 -0700

    8kyu bin to decimal

---
+++ CC 2022-09-25/cw.js
@@ -0,0 +1,7 @@
+// 8kyu bin to decimal
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+let binToDec = str => parseInt(str, 2)
+
+console.log(binToDec('1001001'))
