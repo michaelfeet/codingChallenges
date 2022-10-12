941da700e46aa2cbc87134cd023f952318f79118
Michael Feet <michael.a.feet@gmail.com>
2022-10-12 13:55:11 -0400

    7 kyu Filter Coffee

---
+++ CC 2022-10-12/cw.js
@@ -0,0 +1,25 @@
+// 7 kyu Filter Coffee
+
+// You love coffee and want to know what beans you can afford to buy it.
+
+// The first argument to your search function will be a number which represents your budget.
+
+// The second argument will be an array of coffee bean prices.
+
+// Your 'search' function should return the stores that sell coffee within your budget.
+
+// The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.
+
+
+
+let search = (budget, prices) => {
+    let affordable = []
+    prices.forEach(price => {
+        if(price <= budget){
+            affordable.push(price)
+        }
+    })
+    return affordable.sort((a, b) => a-b).toString()
+}
+
+console.log(search(14, [7, 3, 23, 9, 14, 20, 7]))
